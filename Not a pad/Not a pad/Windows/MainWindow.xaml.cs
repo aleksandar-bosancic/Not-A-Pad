a1a2c2931a1c6d13547476e66e216a3514a7b7a1
Bosančić Aleksandar <34287315+RockBobHardPants@users.noreply.github.com>
2021-12-07 16:46:58 +0100

    Everything works now
    Everything works, just some fine details left to work out

---
+++ Not a pad/Not a pad/Windows/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Not_a_pad.Annotations;
+
+namespace Not_a_pad.Windows
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public ObservableCollection<NoteWindow> ListOfNotes { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListOfNotes = new ObservableCollection<NoteWindow>();
+            DataContext = this;
+        }
+
+        private void New_Note(object sender, RoutedEventArgs e)
+        {
+            this.ListBox.SelectedItem = null;
+            int count = ListOfNotes.Count;
+            NoteWindow noteWindow = new NoteWindow("Note " + count);
+            ListOfNotes.Add(noteWindow);
+            foreach (var not in ListOfNotes)
+            {
+                Trace.WriteLine(not.Label + " " + not.Brush.ToString());
+            }
+            noteWindow.Show();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void PinButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedNote = (NoteWindow)ListBox.SelectedItem;
+            selectedNote?.Show();
+        }
+
+        private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedNote = (NoteWindow)ListBox.SelectedItem;
+            selectedNote.Hide();
+            ListOfNotes.Remove(selectedNote);
+        }
+
+        private void ColorCombobox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedNote = (NoteWindow)ListBox.SelectedItem;
+            if (sender is ComboBox combo && selectedNote != null)
+            {
+                var brush = combo.SelectedItem as SolidColorBrush;
+                selectedNote.Brush = brush;
+                selectedNote.SetNoteColor(brush);
+            }
+        }
+    }
+}
---
+++ Not a pad/Not a pad/Windows/NoteWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Not_a_pad.Annotations;
+
+namespace Not_a_pad.Windows
+{
+    /// <summary>
+    /// Interaction logic for NoteWindow.xaml
+    /// </summary>
+    public partial class NoteWindow : INotifyPropertyChanged
+    {
+        public string Text { get; set; }
+        public string Label { get; set; }
+
+        private SolidColorBrush _brush;
+
+        public SolidColorBrush Brush
+        {
+            get => _brush;
+
+            set
+            {
+                if (value == _brush) return;
+                _brush = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NoteWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public NoteWindow(string label)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Label = label;
+            Brush = (SolidColorBrush) new BrushConverter().ConvertFrom("#feff9c");
+            SetNoteColor(Brush);
+        }
+
+        public void SetNoteColor(SolidColorBrush brush)
+        {
+            Brush = brush;
+            OnPropertyChanged();
+            GradientStop colorStop = new()
+            {
+                Color = brush.Color,
+                Offset = 0.80
+            };
+            GradientStop lightStop = new()
+            {
+                Color = Colors.White,
+                Offset = 0.1
+            };
+            LinearGradientBrush linearGradient = new();
+            linearGradient.GradientStops.Add(colorStop);
+            linearGradient.GradientStops.Add(lightStop);
+            this.NoteTextBox.Background = linearGradient;
+            this.NoteLabel.Background = brush;
+        }
+
+        private void NoteLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void NoteTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NoteLabel_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var clickedLabel = sender as Label;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void NoteWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void NoteTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Text = new TextRange(NoteTextBox.Document.ContentStart, NoteTextBox.Document.ContentEnd).Text;
+        }
+
+        private void ButtonB_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
